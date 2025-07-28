4a50e181aa1f6475b23149ad056b0f79248b69ed
Numpkens <numpkins1228@gmail.com>
2025-07-28 17:25:16 +0400

    adjust styling to match the figma file

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import { menuArr } from './menuArray.js'
-const menuHtml = []
-menuArr.forEach(function(item){
-  menuHtml.push(`
-    <section class= "card">
-        <div class="card=start">
-          <img src="/images/${item.image}">
-        </div>
-        <div class="card-mid">
-          <h4 class="card-title">${item.title}</h4>
-          <p class="card-desc">${item.decription}</p>
-        </div>
-        <div class="card-end">
-          <p class="card-menu">...</p>
-        </div>
-      </section>
-    ') 
-}).join('')
 
-document.getElementById('menu-container').innerHTML = menuHtml
+const menuHtml = []
+
+menuArr.forEach(function(item) {
+  menuHtml.push(`
+    <div class="food-container">
+        <img src="${item.image}" class="food-image">
+        <div class="food-details">
+          <h4 class="food-name">${item.title}</h4>
+          <p class="food-description">${item.decription}</p>
+          <p class="food-price">${item.cost}</p>
+        </div>
+        <button class="order-btn">+</button>
+      </div>
+  `)
+})
+
+document.getElementById('menu-container').innerHTML = menuHtml.join('')
+
+console.log(menuHtml)
