5aca591debf8684cb9b0012646459807c2d6d0b0
Numpkens <numpkins1228@gmail.com>
2025-07-28 14:00:52 +0400

    Update css html and add menu array

---
+++ menuArray.js
@@ -0,0 +1,41 @@
+export const = menuArr [
+  {
+    item: "",
+    decription: "",
+    cost: "",
+    image: ""
+  },
+  {
+    item: " ",
+    decription: " ",
+    cost: " ",
+    image: " "
+  },
+  {
+    item: " ",
+    decription: " ",
+    cost: " ",
+    image: " "
+  },
+  {
+    item: " ",
+    decription: " ",
+    cost: " ",
+    image: " "
+  },
+  {
+    item: " ",
+    decription: " ",
+    cost: " ",
+    image: " "
+  },
+  {
+    item: " ",
+    decription: " ",
+    cost: " ",
+    image: " "
+  }
+
+]
+
+Error: Cannot parse given Error object
